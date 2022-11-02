ac55b0ef1ed7d70909dc0918675295cfb0f84de8
Brittany Ellich <brittanyellich@github.com>
2022-11-01 21:57:59 -0700

    Initial phaser setup

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import * as Phaser from 'phaser';
+import Scenes from './scenes';
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Cloud Tycoon',
+
+  type: Phaser.AUTO,
+
+  scale: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+
+  scene: Scenes,
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
+
+  parent: 'game',
+  backgroundColor: '#000000',
+};
+
+export const game = new Phaser.Game(gameConfig);
+
+window.addEventListener('resize', () => {
+  game.scale.refresh();
+});
---
+++ src/scenes/main-menu-scene.ts
@@ -0,0 +1,32 @@
+import { MenuButton } from '../ui/menu-button';
+
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+  active: false,
+  visible: false,
+  key: 'MainMenu',
+};
+
+/**
+ * The initial scene that starts, shows the splash screens, and loads the necessary assets.
+ */
+export class MainMenuScene extends Phaser.Scene {
+  constructor() {
+    super(sceneConfig);
+  }
+
+  public create(): void {
+    this.add
+      .text(100, 50, 'This is a sample main menu. Click the "Start" button below to run your game.', {
+        color: '#FFFFFF',
+      })
+      .setFontSize(24);
+
+    new MenuButton(this, 100, 150, 'Start Game', () => {
+      this.scene.start('Game');
+    });
+
+    new MenuButton(this, 100, 250, 'Settings', () => console.log('settings button clicked'));
+
+    new MenuButton(this, 100, 350, 'Help', () => console.log('help button clicked'));
+  }
+}
