ffa3085a5f73853a8547a2773722df62dd2943c3
Brittany Ellich <brittanyellich@github.com>
2022-11-03 10:57:22 -0700

    Add initial game settings

---
+++ src/config/baseSettings.ts
@@ -0,0 +1,4 @@
+export const baseSettings: GameSettings = {
+    music: true,
+    soundEffects: true
+}
---
+++ src/types/settings.ts
@@ -0,0 +1,5 @@
+// The settings options that a user has chosen
+interface GameSettings {
+    music: boolean; // True meaning music is on, off meaning music is off, true by default
+    soundEffects: boolean; // True meaning sound effects are on, off meaning sound effects are off, true by default
+}
