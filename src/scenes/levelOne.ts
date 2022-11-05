331ce2885f58660269eaa297582fc28436d10136
Ryan Troost <rtroost25@gmail.com>
2022-11-04 22:05:29 -0400

    1st attempt at a repeating timer

--- src/scenes/levelOne.ts
+++ src/scenes/levelOne.ts
@@ -1,3 +1,4 @@
+
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
@@ -6,13 +7,42 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class GameScene extends Phaser.Scene {
 
+  private dateText: Phaser.GameObjects.Text;
+  private moneyText: Phaser.GameObjects.Text;
+  private gameState: GameState;
+
   constructor() {
     super(sceneConfig);
   }
 
-  public create(): void {
+  public init(gameState: GameState): void {
+    this.gameState = gameState;
   }
 
-  public update(): void {
+  public create(): void {
+    this.dateText = this.add.text(100, 200, 'Month ' + this.gameState.currentMonth);
+    this.moneyText = this.add.text(100, 250, 'Money ' + this.gameState.money);
+
+    // Evey 5 seconds update the time
+    this.time.addEvent({
+      delay: 5000,
+      callback: this.updateState,
+      callbackScope: this,
+      loop: true
+    });
+
   }
+
+  public update(): void { }
+
+  private updateState(): void {
+
+    this.gameState.currentMonth++;
+    this.gameState.money += 1000;
+
+    this.dateText.setText('Month ' + this.gameState.currentMonth);
+    this.moneyText.setText('Money ' + this.gameState.money);
+
+  }
+
 }
